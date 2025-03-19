fc031f447e406e897f94db29c611efac84c1f4b2
Vo Song Toan <toan@deltabrains.vn>
2025-03-19 10:03:00 +0700

    Init source api

---
+++ api/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Chỉ dùng khi phát triển
+      }),
+    }),
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ api/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+const corsOptions = {
+  origin: '*', // Cho phép tất cả các nguồn truy cập
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Cho phép các phương thức HTTP
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cho phép các header HTTP
+};
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors(corsOptions);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Loại bỏ các field không mong muốn
+      forbidNonWhitelisted: true, // Trả về lỗi nếu có field không mong muốn
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
