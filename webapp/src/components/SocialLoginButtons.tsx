80b42fd3fb1add3e532807b2de35d94351508def
Vo Song Toan <vosongtoan21@gmail.com>
2025-12-15 20:20:03 +0700

    delete login facebook

--- webapp/src/components/SocialLoginButtons.tsx
+++ webapp/src/components/SocialLoginButtons.tsx
@@ -7,10 +7,6 @@ export default function SocialLoginButtons() {
     window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/auth/google`;
   };
 
-  const handleFacebookLogin = () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/auth/facebook`;
-  };
-
   return (
     <div className='flex flex-col gap-3'>
       <Button
@@ -36,16 +32,6 @@ export default function SocialLoginButtons() {
           />
         </svg>
         Đăng nhập với Google
-      </Button>
-
-      <Button
-        onClick={handleFacebookLogin}
-        className='w-full flex items-center justify-center gap-2'
-      >
-        <svg className='w-5 h-5' fill='#1877F2' viewBox='0 0 24 24'>
-          <path d='M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z' />
-        </svg>
-        Đăng nhập với Facebook
       </Button>
     </div>
   );
